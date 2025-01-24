ad7c5de4c13bf4a7fc97f81ced30baa2116befd3
Basavaraj Kittali <basavaraj.kittali@popreach.com>
2025-01-24 17:51:29 +0000

    deleted provifer.tf and modified main.tf

--- php-nginx/terraform/main.tf
+++ php-nginx/terraform/main.tf
@@ -5,6 +5,10 @@ provider "aws" {
 resource "aws_instance" "dockerhost" {
   ami           = "ami-0df8c184d5f6ae949" # Replace with a valid AMI ID
   instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+  # Associate the security group
+  security_groups = [aws_security_group.docker_sg.id]
+  iam_instance_profile = aws_iam_instance_profile.ecs_ecr_instance_profile.name
   key_name      = "won_ls_key"
 
   tags = {
